b1693c3e1e4b839408f7e10e3fb847b67927a733
Suraj Mhatre <surajmcodes@gmail.com>
2025-07-19 11:40:45 +0530

    feat: search functionality implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useDebounce } from "react-use";
+
 import Search from "./components/Search";
-import { useState } from "react";
-import { useEffect } from "react";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 
@@ -20,12 +21,16 @@ function App() {
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debounceSearchTerm, setDebouceSearchTerm] = useState("");
 
-  const fetchMovies = async () => {
+  useDebounce(() => setDebouceSearchTerm(searchTerm), 500, [searchTerm]);
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
     setErrorMessage("");
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
       if (!response.ok) {
         throw new Error("Failed to fetch movies.");
@@ -46,8 +51,8 @@ function App() {
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(debounceSearchTerm);
+  }, [debounceSearchTerm]);
   return (
     <>
       <div className="pattern" />
